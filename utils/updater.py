6ea38a93cac577eba89b5976c63941fb4eff5a4a
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-04-06 22:00:16 +0430

    update

--- utils/updater.py
+++ utils/updater.py
@@ -1,5 +1,34 @@
-try:
-    import requests
-    print('Im python. I could import requests')
-except e as Exception:
-    print(e)
+import re
+from requests import get
+
+def main():
+    content = ''
+    list_of_posts = []
+    md_pattern = '- [{}](https://alirezayahyapour.pythonanywhere.com/{}/{}/)'
+
+    r = get('https://alirezayahyapour.pythonanywhere.com/api/v1/posts/').json()
+
+    with open('README.md', 'r') as original_file:
+        content = original_file.read()
+
+    for post in r:
+        list_of_posts.append(
+            md_pattern.format(post['title'], post['lang'], post['slug'])
+        )
+
+    with open('README.md', 'w') as f:
+        f.write(
+            re.sub(
+                r'(<!--POSTS:START-->\n).*?(\n<!--POSTS:END-->)',
+                r'\1{}\2'.format('\n'.join(list_of_posts)),
+                content,
+                flags=re.MULTILINE|re.DOTALL)
+        )
+
+def main2():
+    import os
+    print('READING FROM: {}'.format(os.environ['ENDPOINT']))
+    print('TITLE IS: {}'.format(os.environ['TITLE']))
+    print('URL IS: {}'.format(os.environ['URL']))
+
+main2()
